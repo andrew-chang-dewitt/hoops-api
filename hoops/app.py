96937e1216c90bcc320555fe34ddf8e5581d2294
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-10-19 15:50:15 -0400

    Register transaction Blueprint to app.
    Also change to status route instead of index.

--- hoops/app.py
+++ hoops/app.py
@@ -7,17 +7,24 @@ Run by either calling `python -m hoops.app` or
 © 2021 Andrew Chang-DeWitt
 Source distributed under an MIT License.
 """
+from typing import Any
+
 from flask import Flask
+
+from transactions import transactions
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index() -> dict[str, bool]:
+@app.route("/status")
+def status() -> dict[str, Any]:
     """Check API status at index."""
     return {
-        "status": True
+        "ok": True,
     }
+
+
+app.register_blueprint(transactions)
 
 
 if __name__ == "__main__":
