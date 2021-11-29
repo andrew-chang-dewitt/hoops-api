fee83c1ad97d54450681b1760b3a68e9935f4d13
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-11-29 13:50:33 -0500

    Test full functionality for creating envelopes.

--- tests/integration/test_envelope.py
+++ tests/integration/test_envelope.py
@@ -47,6 +47,41 @@ class TestRoutePostRoot(TestCase):
                     msg="Responds with a status code of 201."):
                 self.assertEqual(201, response.status_code)
 
+            with self.subTest(
+                    msg="Responds with newly created Envelope's data."):
+                body = response.json()
+
+                with self.subTest(msg="Saves the given name."):
+                    self.assertEqual(body["name"], new_envelope["name"])
+                with self.subTest(msg="Bound to current user in auth token."):
+                    self.assertEqual(body["user_id"], str(user_id))
+                with self.subTest(msg="Has a UUID identifier."):
+                    self.assertTrue(UUID(body["id"]))
+                with self.subTest(msg="Starts with zero funds."):
+                    self.assertEqual(body["total_funds"], 0)
+
+            with self.subTest(msg="Saves the Envelope to the database."):
+                body = response.json()
+                new_id = UUID(body["id"])
+
+                await database.connect()
+                query_result = await database.execute_and_return(sql.SQL("""
+                    SELECT * FROM envelope
+                    WHERE id = {new_id};
+                """).format(new_id=sql.Literal(new_id)))
+                await database.disconnect()
+
+                result = query_result[0]
+
+                with self.subTest(
+                    msg="Given envelope name & database envelope name match."
+                ):
+                    self.assertEqual(result["name"], new_envelope["name"])
+                with self.subTest(msg="Binds to currently auth'd user."):
+                    self.assertEqual(result["user_id"], user_id)
+                with self.subTest(msg="Starts with 0 funds."):
+                    self.assertEqual(result["total_funds"], 0)
+
 
 if __name__ == "__main__":
     main()
