fde818959084649aa1460c19ccb89a18bbc2e90a
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-12-01 15:13:55 -0500

    Impl GET /balance/available & /balance/envelope/id
    Breaks Transaction tests, needs fixing. Adds fields for tracking
    spent_from on Transactions, but doesn't do anything with it other than
    calculate balances. Needs added to TransactionBase & then methods &
    routes added as appropriate for features.

--- src/models/z_relations.sql
+++ src/models/z_relations.sql
@@ -1,17 +1,23 @@
-ALTER TABLE "envelope" 
-ADD CONSTRAINT fk_user
-    FOREIGN KEY(user_id)
-        REFERENCES hoops_user(id)
-        ON DELETE CASCADE;
+ALTER TABLE "envelope"
+    ADD CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES hoops_user(id)
+            ON DELETE CASCADE;
 
-ALTER TABLE "account" 
-ADD CONSTRAINT fk_user
-    FOREIGN KEY(user_id)
-        REFERENCES hoops_user(id)
-        ON DELETE CASCADE;
+ALTER TABLE "account"
+    ADD CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES hoops_user(id)
+            ON DELETE CASCADE;
 
-ALTER TABLE "transaction" 
-ADD CONSTRAINT fk_user
-    FOREIGN KEY(account_id)
-        REFERENCES account(id)
-        ON DELETE CASCADE;
+ALTER TABLE "transaction"
+    ADD CONSTRAINT fk_account
+        FOREIGN KEY(account_id)
+            REFERENCES account(id)
+            ON DELETE CASCADE;
+
+ALTER TABLE "transaction"
+    ADD CONSTRAINT fk_spent_from
+        FOREIGN KEY(spent_from)
+            REFERENCES envelope(id)
+            ON DELETE SET NULL;
--- tests/integration/test_balance.py
+++ tests/integration/test_balance.py
@@ -1,7 +1,8 @@
 """Tests for /balance routes."""
 
+from datetime import datetime
 from decimal import Decimal
-from typing import Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional
 from unittest import main, IsolatedAsyncioTestCase as TestCase
 from uuid import UUID
 
@@ -12,83 +13,84 @@ from tests.helpers.application import (
     get_test_client,
     get_token_header,
 )
+from tests.helpers.database import setup_user, setup_account
 from src.database import Client
 
 BASE_URL = "/balance"
 
 
-async def setup_user(database: Client) -> Tuple[UUID, UUID]:
-    """Sets up database with 2 users for testing."""
-    query = """
-        INSERT INTO
-            hoops_user(handle, full_name, preferred_name, password)
-        VALUES
-            ('user', 'A Full Name', 'Nickname', '@ new p4s5w0rd'),
-            ('other', 'Other', 'Other', 'other')
-        RETURNING id;
-    """
-
-    await database.connect()
-    result: List[Dict[str, UUID]] = await database.execute_and_return(query)
-    await database.disconnect()
-
-    return result[0]["id"], result[1]["id"]
-
-
-async def setup_account(
-    database: Client,
-    user_id: Optional[UUID] = None
-) -> UUID:
-    """Sets up database with 1 account for given user for testing."""
-    if not user_id:
-        user_id = (await setup_user(database))[0]
-
-    query = sql.SQL("""
-        INSERT INTO account(user_id, name)
-        VALUES ({user_id}, {name})
-        RETURNING id;
+def _build_transaction(
+    account_id: UUID,
+    amount: Decimal = Decimal("1.00"),
+    payee: str = "payee",
+    description: str = "description",
+    timestamp: datetime = datetime.fromisoformat("2019-12-10T08:12-05:00"),
+    spent_from: Optional[UUID] = None,
+) -> sql.Composed:
+    return sql.SQL("""
+        ({amount},
+         {payee},
+         {description},
+         {timestamp},
+         {account_id},
+         {spent_from})
     """).format(
-        user_id=sql.Literal(user_id),
-        name=sql.Literal("an account"))
-
-    await database.connect()
-    result: List[Dict[str, UUID]] = await database.execute_and_return(query)
-    await database.disconnect()
-
-    return result[0]["id"]
+        account_id=sql.Literal(account_id),
+        amount=sql.Literal(amount),
+        payee=sql.Literal(payee),
+        description=sql.Literal(description),
+        timestamp=sql.Literal(timestamp),
+        spent_from=sql.Literal(spent_from))
 
 
 async def setup_transactions(
     database: Client,
-    amounts: List[Decimal],
-    account_id: Optional[UUID] = None,
+    values: List[Dict[str, Any]],
+    account_id: UUID,
 ) -> None:
-    """Sets up database with transactions of given amounts."""
-    if not account_id:
-        account_id = await setup_account(database)
-
-    def build_transaction(amount: Decimal) -> sql.Composed:
-        return sql.SQL("""
-            ({amount},
-             'payee',
-             'description',
-             '2019-12-10T08:12-05:00',
-             {account_id})
-        """).format(
-            amount=sql.Literal(amount),
-            account_id=sql.Literal(account_id))
-
+    """Sets up database with transactions of given properties."""
     query = sql.SQL("""
         INSERT INTO
-            transaction(amount, payee, description, timestamp, account_id)
-        VALUES {amounts};
+            transaction(
+                amount,
+                payee,
+                description,
+                timestamp,
+                account_id,
+                spent_from)
+        VALUES {transactions};
     """).format(
-        amounts=sql.SQL(",").join(
-            [build_transaction(amount) for amount in amounts]))
+        transactions=sql.SQL(",").join(
+            [_build_transaction(account_id, **value) for value in values]))
 
     await database.connect()
     await database.execute(query)
     await database.disconnect()
+
+
+async def setup_envelope(
+        database: Client,
+        user_id: UUID,
+        name: str = "name",
+        funds: Decimal = Decimal(0),
+) -> UUID:
+    """Sets up database with Envelopes using given name & user."""
+    query = sql.SQL("""
+        INSERT INTO envelope(name, user_id, total_funds)
+        VALUES ({name}, {user_id}, {total_funds})
+        RETURNING id;
+    """).format(
+        name=sql.Literal(name),
+        user_id=sql.Literal(user_id),
+        total_funds=sql.Literal(funds))
+
+    await database.connect()
+    query_result = await database.execute_and_return(query)
+    await database.disconnect()
+
+    result: UUID = query_result[0]["id"]
+
+    return result
 
 
 class TestRouteGetTotal(TestCase):
@@ -100,21 +102,21 @@ class TestRouteGetTotal(TestCase):
             client, database = clients
 
             # insert some test transactions
-            user_id = (await setup_user(database))[0]
+            user_id = await setup_user(database)
             account1_id = await setup_account(database, user_id)
             await setup_transactions(
                 database,
                 [
-                    Decimal(1),
-                    Decimal(2),
-                    Decimal(3),
-                    Decimal(4),
-                    Decimal(5),
-                    Decimal(6),
-                    Decimal(7),
-                    Decimal(8),
-                    Decimal(9),
-                    Decimal(10),
+                    {"amount": Decimal(1)},
+                    {"amount": Decimal(2)},
+                    {"amount": Decimal(3)},
+                    {"amount": Decimal(4)},
+                    {"amount": Decimal(5)},
+                    {"amount": Decimal(6)},
+                    {"amount": Decimal(7)},
+                    {"amount": Decimal(8)},
+                    {"amount": Decimal(9)},
+                    {"amount": Decimal(10)},
                 ],
                 account1_id)
 
@@ -122,16 +124,16 @@ class TestRouteGetTotal(TestCase):
             await setup_transactions(
                 database,
                 [
-                    Decimal(11),
-                    Decimal(12),
-                    Decimal(13),
-                    Decimal(14),
-                    Decimal(15),
-                    Decimal(16),
-                    Decimal(17),
-                    Decimal(18),
-                    Decimal(19),
-                    Decimal(20),
+                    {"amount": Decimal(11)},
+                    {"amount": Decimal(12)},
+                    {"amount": Decimal(13)},
+                    {"amount": Decimal(14)},
+                    {"amount": Decimal(15)},
+                    {"amount": Decimal(16)},
+                    {"amount": Decimal(17)},
+                    {"amount": Decimal(18)},
+                    {"amount": Decimal(19)},
+                    {"amount": Decimal(20)},
                 ],
                 account2_id)
 
@@ -162,21 +164,21 @@ class TestRouteGetAccount(TestCase):
             client, database = clients
 
             # insert some test transactions
-            user_id = (await setup_user(database))[0]
+            user_id = await setup_user(database)
             account_id = await setup_account(database, user_id)
             await setup_transactions(
                 database,
                 [
-                    Decimal(1),
-                    Decimal(2),
-                    Decimal(3),
-                    Decimal(4),
-                    Decimal(5),
-                    Decimal(6),
-                    Decimal(7),
-                    Decimal(8),
-                    Decimal(9),
-                    Decimal(10),
+                    {"amount": Decimal(1)},
+                    {"amount": Decimal(2)},
+                    {"amount": Decimal(3)},
+                    {"amount": Decimal(4)},
+                    {"amount": Decimal(5)},
+                    {"amount": Decimal(6)},
+                    {"amount": Decimal(7)},
+                    {"amount": Decimal(8)},
+                    {"amount": Decimal(9)},
+                    {"amount": Decimal(10)},
                 ],
                 account_id)
 
@@ -200,6 +202,105 @@ class TestRouteGetAccount(TestCase):
                 body = response.json()
 
                 self.assertEqual(body["amount"], 55)
+
+
+class TestRouteGetEnvelope(TestCase):
+    """Testing GET /balance/envelope."""
+
+    async def test_valid_request(self) -> None:
+        """Testing a valid request's response."""
+        async with get_test_client() as clients:
+            client, database = clients
+
+            # insert some test transactions
+            user_id = await setup_user(database)
+            account_id = await setup_account(database, user_id)
+            envelope1 = await setup_envelope(database,
+                                             user_id,
+                                             funds=Decimal(100))
+            await setup_transactions(
+                database,
+                [
+                    {"amount": Decimal(-1), "spent_from": envelope1},
+                    {"amount": Decimal(-2), "spent_from": envelope1},
+                    {"amount": Decimal(-3), "spent_from": envelope1},
+                    {"amount": Decimal(-4), "spent_from": envelope1},
+                    {"amount": Decimal(-5), "spent_from": envelope1},
+                    {"amount": Decimal(-6), "spent_from": envelope1},
+                    {"amount": Decimal(-7), "spent_from": envelope1},
+                    {"amount": Decimal(-8), "spent_from": envelope1},
+                    {"amount": Decimal(-9), "spent_from": envelope1},
+                    {"amount": Decimal(-10), "spent_from": envelope1},
+                ],
+                account_id)
+
+            response = await client.get(
+                f"{BASE_URL}/envelope/{envelope1}",
+                headers={
+                    **get_token_header(user_id),
+                    "accept": "application/json"})
+
+            with self.subTest(
+                    msg="Responds with a status code of 200."):
+                self.assertEqual(200, response.status_code)
+
+            with self.subTest(
+                msg="Balance is total funds - sum of Transactions spent."
+            ):
+                body = response.json()
+                print(f"body: {body}")
+
+                self.assertEqual(body["amount"], 45)
+
+
+class TestRouteGetAvailable(TestCase):
+
+    """Testing GET /balance/available."""
+
+    async def test_valid_request(self) -> None:
+        """Testing a valid request's response."""
+        async with get_test_client() as clients:
+            client, database = clients
+
+            # insert some test transactions
+            user_id = await setup_user(database)
+            account_id = await setup_account(database, user_id)
+            envelope1 = await setup_envelope(database, user_id, funds=Decimal(5))
+            envelope2 = await setup_envelope(database, user_id, funds=Decimal(10))
+            await setup_transactions(
+                database,
+                [
+                    {"amount": Decimal(-1), "spent_from": envelope1},
+                    {"amount": Decimal(-2)},
+                    {"amount": Decimal(-3)},
+                    {"amount": Decimal(-4)},
+                    {"amount": Decimal(-5)},
+                    {"amount": Decimal(-6)},
+                    {"amount": Decimal(-7)},
+                    {"amount": Decimal(-8)},
+                    {"amount": Decimal(-9)},
+                    {"amount": Decimal(-10), "spent_from": envelope2},
+                    {"amount": Decimal(100)},
+                ],
+                account_id)
+
+            response = await client.get(
+                f"{BASE_URL}/available",
+                headers={
+                    **get_token_header(user_id),
+                    "accept": "application/json"})
+
+            with self.subTest(
+                    msg="Responds with a status code of 200."):
+                self.assertEqual(200, response.status_code)
+
+            with self.subTest(
+                msg="Available balance is total - unspent amount in Envelopes."
+            ):
+                body = response.json()
+                print(f"body: {body}")
+
+                self.assertEqual(body["amount"], 41)
 
 
 if __name__ == "__main__":
